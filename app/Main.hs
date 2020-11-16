4c1c6a6fd5844ee63d67b36373b7a5076f360d06
Mac <bagwellmac@gmail.com>
2020-11-16 16:53:26 -0600

    Output equivalence class outcome distributions

--- app/Main.hs
+++ app/Main.hs
@@ -1,54 +1,30 @@
 module Main where
 
-import qualified Data.Set as Set
-import Data.List.Unique
 import Data.List
-import Control.Parallel
-import Control.Parallel.Strategies
+import Data.Text (Text)
+import qualified Data.ByteString.Lazy as BSL
+import Data.Csv
 
 import PokerCards
 import PokerEvaluator
 import PokerHandsList
+import PokerWinProbabilities
+
+data OutcomeRecord = OutcomeRecord (EquivalenceClass, (Float, Float, Float))
+
+instance ToRecord OutcomeRecord where
+    toRecord (OutcomeRecord (ec, (w, t, l))) = record [toField (show ec), toField w, toField t, toField l]
 
 kNumSimulations :: Int
-kNumSimulations = 1000000
+kNumSimulations = 10000
 
-kCard1 = Card Ace Spades
-kCard2 = Card King Spades
-kCard3 = Card Eight Spades
-kCard4 = Card Eight Hearts
+kSeed :: Int
+kSeed = 42
 
-simulateHand :: EquivalenceClass -> Int -> Ordering
-simulateHand ec n = evaluate (a1 : a2 : board) (b1 : b2 : board)
-    where
-        board = take 5 $ newDeckWithOut n [a1, a2, b1, b2]
-        a1 = (fst . fst) ec; a2 = (snd . fst) ec; b1 = (fst . snd) ec; b2 = (snd . snd) ec
-
-pSimulateHands :: EquivalenceClass -> [Int] -> [Ordering]
-pSimulateHands ec ns = parMap rpar (simulateHand ec) ns
-
-sumResults :: [Ordering] -> (Int, Int, Int)
-sumResults results = aux results (0, 0, 0)
-    where
-        aux :: [Ordering] -> (Int, Int, Int) -> (Int, Int, Int)
-        aux [] (w, t, l) = (w, t, l)
-        aux (r:rs) (w, t, l)
-            | r == GT = aux rs (w + 1, t, l)
-            | r == LT = aux rs (w, t, l + 1)
-            | otherwise = aux rs (w, t + 1, l)
-    
-means :: (Int, Int, Int) -> (Float, Float, Float)
-means (w, t, l) = (w' / s, t' / s, l' / s)
-    where 
-        w' = fromIntegral w :: Float
-        t' = fromIntegral t :: Float
-        l' = fromIntegral l :: Float
-        s = w' + t' + l'
-
-meanResults :: [Ordering] -> (Float, Float, Float)
-meanResults = means . sumResults
+kOutFile :: [Char]
+kOutFile = "data/outcome-distributions.csv"
 
 main :: IO ()
 main = do
-    let ec = equivClass (kCard1, kCard2) (kCard3, kCard4)
-    putStrLn $ show $ meanResults $ pSimulateHands ec [1..kNumSimulations]
+    BSL.writeFile kOutFile $ encode $ map OutcomeRecord $ outcomeDistributions [kSeed .. ] kNumSimulations
+--    putStrLn $ show $ take 40 $ outcomeDistributions [kSeed ..] kNumSimulations
---
+++ src/PokerWinProbabilities.hs
@@ -0,0 +1,48 @@
+module PokerWinProbabilities where
+
+import Data.List.Unique
+import Data.List
+import qualified Data.Set as Set
+import Control.Parallel
+import Control.Parallel.Strategies
+
+import PokerCards
+import PokerEvaluator
+import PokerHandsList
+
+simulateHand :: EquivalenceClass -> Int -> Ordering
+simulateHand ec n = evaluate (a1 : a2 : board) (b1 : b2 : board)
+    where
+        board = take 5 $ newDeckWithOut n [a1, a2, b1, b2]
+        a1 = (fst . fst) ec; a2 = (snd . fst) ec; b1 = (fst . snd) ec; b2 = (snd . snd) ec
+
+pSimulateHands :: EquivalenceClass -> [Int] -> [Ordering]
+pSimulateHands ec ns = parMap rpar (simulateHand ec) ns
+
+sumResults :: [Ordering] -> (Int, Int, Int)
+sumResults results = aux results (0, 0, 0)
+    where
+        aux :: [Ordering] -> (Int, Int, Int) -> (Int, Int, Int)
+        aux [] (w, t, l) = (w, t, l)
+        aux (r:rs) (w, t, l)
+            | r == GT = aux rs (w + 1, t, l)
+            | r == LT = aux rs (w, t, l + 1)
+            | otherwise = aux rs (w, t + 1, l)
+    
+means :: (Int, Int, Int) -> (Float, Float, Float)
+means (w, t, l) = (w' / s, t' / s, l' / s)
+    where 
+        w' = fromIntegral w :: Float
+        t' = fromIntegral t :: Float
+        l' = fromIntegral l :: Float
+        s = w' + t' + l'
+
+meanResults :: [Ordering] -> (Float, Float, Float)
+meanResults = means . sumResults
+
+computeOutcomeDistribution :: EquivalenceClass -> Int -> Int -> (Float, Float, Float)
+computeOutcomeDistribution ec seed n = meanResults $ pSimulateHands ec [seed .. (seed + n - 1)]
+
+outcomeDistributions :: [Int] -> Int -> [(EquivalenceClass, (Float, Float, Float))]
+outcomeDistributions seeds n = map (\(seed, ec) -> (ec, computeOutcomeDistribution ec seed n)) (zip seeds (Set.toList allEquivClasses))
+
